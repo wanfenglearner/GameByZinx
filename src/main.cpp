d558fd06aa2342c4b5d30d2e5cc9ae865afea8b1
robin <2200726897@qq.com>
2025-06-29 11:21:51 +0800

    2025-6-29

--- src/main.cpp
+++ src/main.cpp
@@ -3,61 +3,6 @@
 #include <memory>
 #include "aoiworld.hpp"
 
-class PlayerX : public Player {
-
-public:
-
-    PlayerX(int x, int y, std::string name):
-        Player(x, y, name)
-    {
-
-    }
-        // 获得X坐标
-    int getX()
-    {
-        return m_x;
-    }
-
-    // 获得Y坐标
-    int getY() {
-        return m_y;
-    }
-
-    // 获得名字
-    std::string getName() {
-        return m_name;
-    }
-};
-
-
-
-
-void test() {
-
-    // AOIWorld aoi(20, 200, 50, 230, 6, 6);
-
-    // auto p1 = std::make_shared<PlayerX>(60, 107, "玩家1");
-    // auto p2 = std::make_shared<PlayerX>(91, 118, "玩家2");
-    // auto p3 = std::make_shared<PlayerX>(147, 133, "玩家3");
-
-    // aoi.addAOIPlayer(p1.get());
-    // aoi.addAOIPlayer(p2.get());
-    // aoi.addAOIPlayer(p3.get());
-
-    // for(const auto& i : aoi.getAroundPlayer(p1.get())) {
-    //     std::cout << i->getName() << " x:" << i->getX() << " y:" << i->getY() << std::endl; 
-    // }
-
-    // // for(const auto& i : aoi.getAroundPlayer(p2.get())) {
-    // //     std::cout << i->getName() << " x:" << i->getX() << " y:" << i->getY() << std::endl; 
-    // // }
-
-    // for(const auto& i : aoi.getAroundPlayer(p3.get())) {
-    //     std::cout << i->getName() << " x:" << i->getX() << " y:" << i->getY() << std::endl; 
-    // } 
-
-}
-
 int init(int argc, char*argv[]) {
    
     int port = 0;
@@ -85,6 +30,7 @@ int init(int argc, char*argv[]) {
 
     // 结束框架
     ZinxKernel::ZinxKernelFini();
+
 
     return 0;
 
